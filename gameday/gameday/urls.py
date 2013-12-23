88181a11447ad52151ae3b2955b2d9c122bf387a
Curt Merrill <curt@curtmerrill.com>
2013-12-23 10:27:03 -0800

    Trying to fix cache setup

--- gameday/gameday/urls.py
+++ gameday/gameday/urls.py
@@ -5,6 +5,6 @@ from django.views.decorators.cache import cache_page
 from games.views import IndexView
 
 urlpatterns = patterns('',
-    (r'^/?$', cache_page(IndexView.as_view())),
+    (r'^/?$', IndexView.as_view()),
     (r'^about/?$', cache_page(TemplateView.as_view(template_name="about.html"), 60*120)),
 )
--- gameday/games/views.py
+++ gameday/games/views.py
@@ -1,10 +1,22 @@
+from datetime import date
+
 from django.views.generic import ListView
+from django.views.decorators.cache import cache_page
 
 from games.models import Game
 
-from datetime import date
+class CacheViewMixin(object):
+    cache_timeout = 60
 
-class IndexView(ListView):
+    def get_cache_timeout(self):
+        return self.cache_timeout
+
+    def dispatch(self, *args, **kwargs):
+        return cache_page(self.get_cache_timeout())(super(CacheViewMixin, self).dispatch)(*args, **kwargs)
+
+
+class IndexView(CacheViewMixin, ListView):
+    cache_timeout = 60*30
     context_object_name = 'games'
     queryset = Game.objects.filter(date=date.today())
     template_name = 'index.html'
