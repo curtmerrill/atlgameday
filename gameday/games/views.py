d3a837cd3f26781d9317a37002d9db742056e94a
Curt Merrill <curt@curtmerrill.com>
2013-12-24 05:29:02 -0800

    oops

--- gameday/games/views.py
+++ gameday/games/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 
 from games.models import Game
 
-class IndexView(CacheViewMixin, ListView):
+class IndexView(ListView):
     context_object_name = 'games'
     queryset = Game.objects.filter(date=date.today())
     template_name = 'index.html'
