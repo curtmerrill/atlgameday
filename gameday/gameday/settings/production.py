6d154655782ee5ce0483ec1daa46f791e3ff926f
Curt Merrill <curt@curtmerrill.com>
2013-12-25 15:18:17 -0500

    Still fighting cache settings

--- gameday/gameday/settings/production.py
+++ gameday/gameday/settings/production.py
@@ -22,6 +22,8 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 600,
+        'KEY_PREFIX': 'gameday',
     }
 }
 
